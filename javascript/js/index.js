6e5d0389ee9b1f081dec97411cd619dd38f1c027
januariesm <149758199+januariesm@users.noreply.github.com>
2023-11-03 08:58:36 +0300

    Add files via upload

---
+++ javascript/js/index.js
@@ -0,0 +1,7 @@
+function display(){
+    //alert("welcome to our system!");
+    document.getElementById("heading").innerHTML="This is Javascript";
+}
+function hide(){
+    document.getElementById("heading").innerHTML="";
+}
